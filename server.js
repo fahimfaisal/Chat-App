301b93411f54a40c45e520d1a130d3975bd76cb7
fahimfaisal <navid2010ps@gmail.com>
2021-10-28 23:21:16 +1100

    adding callback and simple frontend

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+//importing express module
 var express = require('express')
 var app = express()
 
+//Serving a static file (index.html)
 app.use(express.static(__dirname))
 
+//Creating a localhost port
 var server = app.listen(3000, ()=> {
     console.log('Server is listening to port ', server.address().port)
 })
