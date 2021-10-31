875d28a0fd7788ac9382aa8d60cffcfbe9310403
fahimfaisal <navid2010ps@gmail.com>
2021-11-01 01:27:37 +1100

    Creating GET req

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var app = express()
 //Serving a static file (index.html)
 app.use(express.static(__dirname))
 
+var messages = [
+    {name:'Fahim', message: 'This is Fahim'},
+    {name:'Surayez', message: 'This is Surayez'},
+
+]
+
+//Setting up a route to show the messages
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
 //Creating a localhost port
 var server = app.listen(3000, ()=> {
     console.log('Server is listening to port ', server.address().port)
