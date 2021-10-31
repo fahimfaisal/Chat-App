0bc47c4e76f8c4386f48c4c965a3cc2ca464fc07
fahimfaisal <navid2010ps@gmail.com>
2021-11-01 02:06:27 +1100

    Creating POST req

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 //importing express module
+
 var express = require('express')
+
+
+var bodyParser = require('body-parser')
 var app = express()
 
 //Serving a static file (index.html)
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
     {name:'Fahim', message: 'This is Fahim'},
@@ -16,6 +22,11 @@ app.get('/messages', (req, res) => {
     res.send(messages)
 })
 
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
+})
 //Creating a localhost port
 var server = app.listen(3000, ()=> {
     console.log('Server is listening to port ', server.address().port)
